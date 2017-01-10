672f0f969ce244c411eabafb70b3c960bb09986c
admin <>
2017-01-10 12:06:47 -0200

    {/Relatorio de Frete v03/src/main/java/com/asia/frete/relatoriodefretev03/atividade.java}

--- Relatorio de Frete v03/src/main/java/com/asia/frete/relatoriodefretev03/atividade.java
+++ Relatorio de Frete v03/src/main/java/com/asia/frete/relatoriodefretev03/atividade.java
@@ -12,6 +12,9 @@ public class atividade implements java.io.Serializable
    private com.asia.frete.relatoriodefretev03.pedido pedido_da_atividade;
    private java.util.List<com.asia.frete.relatoriodefretev03.item> item_da_atividade;
 
+   @org.kie.api.definition.type.Label(value = "comentario")
+   private java.lang.String comentario;
+
    public atividade()
    {
    }
@@ -38,12 +41,24 @@ public class atividade implements java.io.Serializable
       this.item_da_atividade = item_da_atividade;
    }
 
+   public java.lang.String getComentario()
+   {
+      return this.comentario;
+   }
+
+   public void setComentario(java.lang.String comentario)
+   {
+      this.comentario = comentario;
+   }
+
    public atividade(
          com.asia.frete.relatoriodefretev03.pedido pedido_da_atividade,
-         java.util.List<com.asia.frete.relatoriodefretev03.item> item_da_atividade)
+         java.util.List<com.asia.frete.relatoriodefretev03.item> item_da_atividade,
+         java.lang.String comentario)
    {
       this.pedido_da_atividade = pedido_da_atividade;
       this.item_da_atividade = item_da_atividade;
+      this.comentario = comentario;
    }
 
 }
