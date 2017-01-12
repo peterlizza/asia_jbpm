dd23ae49d1ce341a30f0564cf5694a7114ba0df4
admin <>
2017-01-12 14:10:46 -0200

    {/Relatorio Frete V04/src/main/java/com/asia/frete/relatoriodefretev03/pedido.java}

--- Relatorio Frete V04/src/main/java/com/asia/frete/relatoriodefretev03/pedido.java
+++ Relatorio Frete V04/src/main/java/com/asia/frete/relatoriodefretev03/pedido.java
@@ -5,28 +5,73 @@ package com.asia.frete.relatoriodefretev03;
  */
 
 @javax.persistence.Entity
-public class pedido implements java.io.Serializable {
+public class pedido implements java.io.Serializable
+{
 
-    static final long serialVersionUID = 1L;
+   static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "PEDIDO_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(name = "PEDIDO_ID_GENERATOR", sequenceName = "PEDIDO_ID_SEQ")
-    private java.lang.Long id;
+   @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "PEDIDO_ID_GENERATOR")
+   @javax.persistence.Id
+   @javax.persistence.SequenceGenerator(name = "PEDIDO_ID_GENERATOR", sequenceName = "PEDIDO_ID_SEQ")
+   private java.lang.Long id;
 
-    public pedido() {
-    }
-    
-    public pedido(java.lang.Long id) {
-        this.id = id;
-    }
+   private int filial_operac;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+   private java.util.Date dt_criacao;
+
+   private java.lang.String descricao;
+
+   public pedido()
+   {
+   }
+
+   public java.lang.Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(java.lang.Long id)
+   {
+      this.id = id;
+   }
+
+   public int getFilial_operac()
+   {
+      return this.filial_operac;
+   }
+
+   public void setFilial_operac(int filial_operac)
+   {
+      this.filial_operac = filial_operac;
+   }
+
+   public java.util.Date getDt_criacao()
+   {
+      return this.dt_criacao;
+   }
+
+   public void setDt_criacao(java.util.Date dt_criacao)
+   {
+      this.dt_criacao = dt_criacao;
+   }
+
+   public java.lang.String getDescricao()
+   {
+      return this.descricao;
+   }
+
+   public void setDescricao(java.lang.String descricao)
+   {
+      this.descricao = descricao;
+   }
+
+   public pedido(java.lang.Long id, int filial_operac,
+         java.util.Date dt_criacao, java.lang.String descricao)
+   {
+      this.id = id;
+      this.filial_operac = filial_operac;
+      this.dt_criacao = dt_criacao;
+      this.descricao = descricao;
+   }
 
 }
